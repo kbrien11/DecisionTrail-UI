f05787197da87246097a8ae10306cf704555dc3f
kbrien11 <kbrien11@gmail.com>
2025-10-14 16:22:15 -0400

    changed ts to js for config file

--- decisiontrail-ui/next.config.js
+++ decisiontrail-ui/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    trailingSlash: false,
-};
+    experimental: {
+        appDir: true
+    }
+}
 
-export default nextConfig;
+module.exports = nextConfig
