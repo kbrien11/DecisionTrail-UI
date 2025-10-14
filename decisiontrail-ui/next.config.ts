93faf2f5479e46c59afc86e84041d8a80698a412
kbrien11 <kbrien11@gmail.com>
2025-10-14 14:56:02 -0400

    Update next.config.ts

--- decisiontrail-ui/next.config.ts
+++ decisiontrail-ui/next.config.ts
@@ -1,19 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/accounts/:path*",
-                destination: "https://decisiontrail.onrender.com/accounts/:path*" // Added /api/ prefix
-            },
-            {
-                source: "/slack/api/:path*",
-                destination: "https://decisiontrail.onrender.com/slack/api/:path*"
-            }
-
-        ];
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: false,
 };
 
 export default nextConfig;
