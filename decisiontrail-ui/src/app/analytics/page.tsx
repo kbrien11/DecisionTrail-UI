669f792e93114523e3a0240a550de737111d7dc8
kbrien11 <kbrien11@gmail.com>
2025-10-14 17:07:14 -0400

    trying new stuff

--- decisiontrail-ui/src/app/analytics/page.tsx
+++ decisiontrail-ui/src/app/analytics/page.tsx
@@ -1,4 +1,5 @@
 // app/analytics/page.tsx
+import dynamic from 'next/dynamic'
 import AuditAnalyticsDashboard from './AuditAnalyticsDashboard'
 
 export default function AnalyticsPage() {
--- decisiontrail-ui/src/app/dashboard/page.tsx
+++ decisiontrail-ui/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 // app/analytics/page.tsx
 import DashboardPage from './DashboardPage'
 
+import dynamic from 'next/dynamic'
 export default function Dashboard() {
     return <DashboardPage />
 }
